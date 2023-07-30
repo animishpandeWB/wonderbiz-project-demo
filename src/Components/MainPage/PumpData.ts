dd15183468745706bc263b38a82525aad1935faa
Animish <animishpande@gmail.com>
2023-07-30 11:27:29 +0530

    On click on pump logs pump data

---
+++ src/Components/MainPage/PumpData.ts
@@ -0,0 +1,51 @@
+interface PumpData {
+    id: number
+    pumpName: string
+    pumpType: string
+    pumpStatus: boolean
+}
+
+const pumps: PumpData[] = [
+    {id: 1, pumpName: "Pump 1", pumpType: "Jet Pump", pumpStatus: true},
+    {id: 2, pumpName: "Pump 2", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 3, pumpName: "Pump 3", pumpType: "Piston Pump", pumpStatus: false},
+    {id: 4, pumpName: "Pump 4", pumpType: "Jet Pump", pumpStatus: true},
+    {id: 5, pumpName: "Pump 5", pumpType: "Centrifugal Pump", pumpStatus: true},
+    {id: 6, pumpName: "Pump 6", pumpType: "Piston Pump", pumpStatus: false},
+    {id: 7, pumpName: "Pump 7", pumpType: "Jet Pump", pumpStatus: true},
+    {id: 8, pumpName: "Pump 8", pumpType: "Piston Pump", pumpStatus: true},
+    {id: 9, pumpName: "Pump 9", pumpType: "Jet Pump", pumpStatus: false},
+    {id: 10, pumpName: "Pump 10", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 11, pumpName: "Pump 11", pumpType: "Jet Pump", pumpStatus: false},
+    {id: 12, pumpName: "Pump 12", pumpType: "Piston Pump", pumpStatus: false},
+    {id: 13, pumpName: "Pump 13", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 14, pumpName: "Pump 14", pumpType: "Piston Pump", pumpStatus: false},
+    {id: 15, pumpName: "Pump 15", pumpType: "Jet Pump", pumpStatus: false},
+    {id: 16, pumpName: "Pump 16", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 17, pumpName: "Pump 17", pumpType: "Jet Pump", pumpStatus: false},
+    {id: 18, pumpName: "Pump 18", pumpType: "Jet Pump", pumpStatus: false},
+    {id: 19, pumpName: "Pump 19", pumpType: "Piston Pump", pumpStatus: false},
+    {id: 20, pumpName: "Pump 20", pumpType: "Piston Pump", pumpStatus: false},
+    {id: 21, pumpName: "Pump 21", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 22, pumpName: "Pump 22", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 23, pumpName: "Pump 23", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 24, pumpName: "Pump 24", pumpType: "Jet Pump", pumpStatus: false},
+    {id: 25, pumpName: "Pump 25", pumpType: "Pison Pump", pumpStatus: false},
+    {id: 26, pumpName: "Pump 26", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 27, pumpName: "Pump 27", pumpType: "Piston Pump", pumpStatus: false},
+    {id: 28, pumpName: "Pump 28", pumpType: "Piston Pump", pumpStatus: false},
+    {id: 29, pumpName: "Pump 29", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 30, pumpName: "Pump 30", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 31, pumpName: "Pump 31", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 32, pumpName: "Pump 32", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 33, pumpName: "Pump 33", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 34, pumpName: "Pump 34", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 35, pumpName: "Pump 35", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 36, pumpName: "Pump 36", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 37, pumpName: "Pump 37", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 38, pumpName: "Pump 38", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 39, pumpName: "Pump 39", pumpType: "Centrifugal Pump", pumpStatus: false},
+    {id: 40, pumpName: "Pump 40", pumpType: "Centrifugal Pump", pumpStatus: false}
+];
+
+export default pumps;
--- src/Components/MainPage/PumpTable.tsx
+++ src/Components/MainPage/PumpTable.tsx
@@ -1,28 +1,29 @@
 import React, {useState} from 'react';
+import pumps from './PumpData';
 
-interface PumpData {
-    id: number
-    pumpName: string
-    pumpType: string
-    pumpStatus: boolean
-}
+// interface PumpData {
+//     id: number
+//     pumpName: string
+//     pumpType: string
+//     pumpStatus: boolean
+// }
 
 const PumpTable = () => {
 
     const [searchInput, setSearchInput] = useState("");
 
-    const pumps: PumpData[] = [
-        {id: 1, pumpName: "Pump 1", pumpType: "Jet Pump", pumpStatus: true},
-        {id: 2, pumpName: "Pump 2", pumpType: "Centrifugal Pump", pumpStatus: false},
-        {id: 3, pumpName: "Pump 3", pumpType: "Piston Pump", pumpStatus: false},
-        {id: 4, pumpName: "Pump 4", pumpType: "Jet Pump", pumpStatus: true},
-        {id: 5, pumpName: "Pump 5", pumpType: "Centrifugal Pump", pumpStatus: true},
-        {id: 6, pumpName: "Pump 6", pumpType: "Piston Pump", pumpStatus: false},
-        {id: 7, pumpName: "Pump 7", pumpType: "Jet Pump", pumpStatus: true},
-        {id: 8, pumpName: "Pump 8", pumpType: "Piston Pump", pumpStatus: true},
-        {id: 9, pumpName: "Pump 9", pumpType: "Jet Pump", pumpStatus: false},
-        {id: 10, pumpName: "Pump 10", pumpType: "Centrifugal Pump", pumpStatus: false}
-    ];
+    // const pumps: PumpData[] = [
+    //     {id: 1, pumpName: "Pump 1", pumpType: "Jet Pump", pumpStatus: true},
+    //     {id: 2, pumpName: "Pump 2", pumpType: "Centrifugal Pump", pumpStatus: false},
+    //     {id: 3, pumpName: "Pump 3", pumpType: "Piston Pump", pumpStatus: false},
+    //     {id: 4, pumpName: "Pump 4", pumpType: "Jet Pump", pumpStatus: true},
+    //     {id: 5, pumpName: "Pump 5", pumpType: "Centrifugal Pump", pumpStatus: true},
+    //     {id: 6, pumpName: "Pump 6", pumpType: "Piston Pump", pumpStatus: false},
+    //     {id: 7, pumpName: "Pump 7", pumpType: "Jet Pump", pumpStatus: true},
+    //     {id: 8, pumpName: "Pump 8", pumpType: "Piston Pump", pumpStatus: true},
+    //     {id: 9, pumpName: "Pump 9", pumpType: "Jet Pump", pumpStatus: false},
+    //     {id: 10, pumpName: "Pump 10", pumpType: "Centrifugal Pump", pumpStatus: false}
+    // ];
 
     const [pumpsData, setPumpsData] = useState(pumps);
 
@@ -31,10 +32,15 @@ const PumpTable = () => {
         setSearchInput(e.value);
     };
 
-    if(searchInput.length > 0) {
-        pumps.filter((pump) => {
-            return pump.pumpName.match(searchInput);
-        });
+    // if(searchInput.length > 0) {
+    //     pumpsData.filter((pump) => {
+    //         getPump = pump.pumpName.match(searchInput);
+    //         return getPump;
+    //     });
+    // }
+
+    function handleClick(pump: Object) {
+        console.log(pump);
     }
 
     return (
@@ -56,17 +62,18 @@ const PumpTable = () => {
                 {
                     pumpsData.map((pump) => {
                         return (
-                        <div>
-                            <tr>
+                        
+                            <tr onClick={() => handleClick(pump)}>
                                 <td>{pump.id}</td>
                                 <td>{pump.pumpName}</td>
                                 <td>{pump.pumpType}</td>
-                                <td>{pump.pumpStatus}</td>
+                                <td>{pump.pumpStatus ? "On" : "Off"}</td>
                             </tr>
-                        </div>
+                        
                     )})
                 }
             </table>
+        
         </div>
     )
 }
